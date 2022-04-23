a0e7c1237c102dca67f2d0c076cea408f5e9524b
lvzhc <396237830@qq.com>
2022-04-23 19:30:36 +0800

    完成云原生训练营2.1课后作业编写http server

--- 2.1/main.go
+++ 2.1/main.go
@@ -6,25 +6,38 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
-// v3
+func Clien_IP(r *http.Request) string {
+	IpAddress := r.Header.Get("X-Real-Ip")
+	if IpAddress == "" {
+		IpAddress = r.Header.Get("X-Forwarded-For")
+	}
+	if IpAddress == "" {
+		IpAddress = r.RemoteAddr
+	}
+	return IpAddress
+}
+
 func main() {
-	// defer fmt.Println("panic退出前处理")
-	mux := http.NewServeMux()
+	mux := mux.NewRouter()
 	mux.Handle("/", &myHandler{})
 	mux.HandleFunc("/version", version)
 	mux.HandleFunc("/healthZ", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "200")
-		// log.Println(r.Host, r.Method, r.Response.StatusCode)
+		w.WriteHeader(http.StatusOK)
+		log.Printf("%s %s %s %d", Clien_IP(r), r.URL, r.Method, 200)
 	})
+	mux.HandleFunc("/{url:.*}", err)
 
 	server := &http.Server{
 		Addr:         ":80",
 		WriteTimeout: time.Second * 3,
 		Handler:      mux,
 	}
-	log.Println("Starting v3 httpserver")
+	log.Println("Starting my httpserver")
 	log.Fatal(server.ListenAndServe())
 }
 
@@ -32,16 +45,26 @@ type myHandler struct {
 }
 
 func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("this is version 3"))
-	log.Println(r.Host, r.URL, r.Method)
+	w.Write([]byte("this is lvzhancheng http server"))
+	log.Printf("%s %s %s %d", Clien_IP(r), r.URL, r.Method, 200)
 }
 func version(w http.ResponseWriter, r *http.Request) {
 	v, exists := os.LookupEnv("VERSION")
 	if exists {
 		w.Header().Add("version", v)
+		w.Write([]byte("VERSION:" + v))
 	} else {
-		w.Header().Add("version", "NULL")
+		os.Setenv("VERSION", "0.0.1")
+		w.Header().Add("version", "0.0.1")
+		w.Write([]byte("VERSION: 0.0.1"))
 	}
-	fmt.Fprintf(w, os.Getenv("GOOS"))
-	log.Println(r.Host, r.URL, r.Method)
+	log.Printf("%s %s %s %d", Clien_IP(r), r.URL, r.Method, 200)
+}
+func err(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	if len(vars) != 0 {
+		w.WriteHeader(404)
+		w.Write(([]byte("404 page not found")))
+		log.Printf("%s %s %s %d", Clien_IP(r), r.URL, r.Method, 404)
+	}
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module 2.1
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0
