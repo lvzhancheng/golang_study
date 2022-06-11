b46462fe0ff89717db3dc211ee8a54334b2a896d
lvzhancheng <396237830@qq.com>
2022-06-11 11:58:33 +0800

    ✨ feat(main.go): 添加http优雅退出逻辑

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -38,6 +41,12 @@ func main() {
 	}
 	log.Println("Starting my httpserver")
 	log.Fatal(server.ListenAndServe())
+	// 监听信号，优雅退出http服务
+	Watch(func() error {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		return server.Shutdown(ctx)
+	})
 }
 
 type myHandler struct {
@@ -71,4 +80,20 @@ func err(w http.ResponseWriter, r *http.Request) {
 		w.Write(([]byte("404 page not found")))
 		log.Printf("%s %s %s %d", Clien_IP(r), r.URL, r.Method, 404)
 	}
+}
+
+// 监听信号
+func Watch(fns ...func() error) {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	//阻塞
+	s := <-ch
+	close(ch)
+	log.Println("接收到信号", s.String(), "执行关闭函数")
+	for i := range fns {
+		if err := fns[i](); err != nil {
+			log.Println(err)
+		}
+	}
+	log.Println("关闭函数执行完成")
 }
